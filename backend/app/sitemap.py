a68d83c098dd27bbbd5909fff828d44ca88cfd43
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2025-09-09 21:16:51 +0500

    Fix sitemap formatting for flake8

--- backend/app/sitemap.py
+++ backend/app/sitemap.py
@@ -12,7 +12,9 @@ def sitemap():
     pages = []
 
     # Статические страницы
-    static_pages = ['main.index', 'main.products', 'main.about', 'main.contacts']
+    static_pages = (
+        'main.index', 'main.products', 'main.about', 'main.contacts'
+    )
     for page in static_pages:
         pages.append({
             'loc': url_for(page, _external=True),
@@ -22,21 +24,26 @@ def sitemap():
     # Динамические страницы продуктов
     for product in Product.query.all():
         pages.append({
-            'loc': url_for('product_detail', product_id=product.id, _external=True),
+            'loc': url_for(
+                'product_detail', product_id=product.id, _external=True
+            ),
             'lastmod': datetime.now().date()
         })
 
-    # 3. Генерация XML
+    # Генерация XML
     sitemap_xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
-    sitemap_xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    sitemap_xml += (
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    )
 
     for page in pages:
-        sitemap_xml += f'  <url>\n'
-        sitemap_xml += f'    <loc>{page["loc"]}</loc>\n'
-        sitemap_xml += f'    <lastmod>{page["lastmod"]}</lastmod>\n'
-        sitemap_xml += f'    <changefreq>monthly</changefreq>\n'
-        sitemap_xml += f'    <priority>0.8</priority>\n'
-        sitemap_xml += f'  </url>\n'
+        sitemap_xml += f"""  <url>
+        <loc>{page["loc"]}</loc>
+        <lastmod>{page["lastmod"]}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    """
 
     sitemap_xml += '</urlset>'
 
